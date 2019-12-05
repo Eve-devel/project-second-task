ccf5ca0e22c7db6192c47c7551aeb2d5f83a5531
Lena <pechorin15@yandex.ru>
2019-12-05 16:46:17 +0400

    Ended guests and rooms dropdowns

--- src/plagins/dropdown.js
+++ src/plagins/dropdown.js
@@ -1,33 +1,66 @@
 $(document).ready(function() {
-  $('.iqdropdown').iqDropdown({
-
-  maxItems: Infinity,
-  minItems: 0,
-  selectionText: 'гость',
-  selectionText1: 'гостя',
-  selectionText2: 'гостей',
-  textPlural: 'Сколько гостей',
-  controls: {
-    position: 'right',
-    displayCls: 'iqdropdown-item-display',
-    controlsCls: 'iqdropdown-item-controls',
-    counterCls: 'counter'
-  },
-  onChange: function(id, count, totalItems) {
-  console.log(id, count, totalItems);
-  },
-  beforeDecrement: function(id, itemCount) {
-    if ((id == "item1") && (itemCount.item2 > 0 || itemCount.item3 > 0) && (itemCount.item1 == 1)) {
-      return false;
+  $('.guests').iqDropdown({
+    maxItems: Infinity,
+    minItems: 0,
+    selectionText: 'гость',
+    selectionText1: 'гостя',
+    selectionText2: 'гостей',
+    selectionText3: 'младенец',
+    selectionText4: 'младенца',
+    selectionText5: 'младенцев',
+    textPlural: 'Сколько гостей',
+    controls: {
+      position: 'right',
+      displayCls: 'iqdropdown-item-display',
+      controlsCls: 'iqdropdown-item-controls',
+      counterCls: 'counter'
+    },
+    onChange: function(id, count, totalItems) {
+      console.log(id, count, totalItems);
+    },
+    beforeDecrement: function(id, itemCount) {
+      if ((id == "item1") && (itemCount.item2 > 0 || itemCount.item3 > 0) && (itemCount.item1 == 1)) {
+        return false;
+      }
+      return true;
+    },
+    beforeIncrement: function(id, itemCount) {
+      if ((id === "item2" || id === "item3") && itemCount.item1 == 0) {
+        return false;
+      }
+      return true;
     }
-    return true;
-  },
-  beforeIncrement: function(id, itemCount) {
-    if ((id === "item2" || id === "item3") && itemCount.item1 == 0) {
-      return false;
+  });
+  $('.rooms').iqDropdown({
+    maxItems: Infinity,
+    minItems: 0,
+    selectionText: 'комната',
+    selectionText1: 'комнаты',
+    selectionText2: 'комнат',
+    selectionText3: 'кровать...',
+    selectionText4: 'кровати...',
+    selectionText5: 'кроватей...',
+    textPlural: 'Сколько комнат',
+    controls: {
+      position: 'right',
+      displayCls: 'iqdropdown-item-display',
+      controlsCls: 'iqdropdown-item-controls',
+      counterCls: 'counter'
+    },
+    onChange: function(id, count, totalItems) {
+      console.log(id, count, totalItems);
+    },
+    beforeDecrement: function(id, itemCount) {
+      if ((id == "item1") && (itemCount.item2 > 0 || itemCount.item3 > 0) && (itemCount.item1 == 1)) {
+        return false;
+      }
+      return true;
+    },
+    beforeIncrement: function(id, itemCount) {
+      if ((id === "item2" || id === "item3") && itemCount.item1 == 0) {
+        return false;
+      }
+      return true;
     }
-    return true;
-  }
-
-});
+  });
 });
