6e2ff40397fd366d32b3b6c6dffae0a94346b86a
Lena <pechorin15@yandex.ru>
2019-12-15 00:57:17 +0400

    Added imask plagin

---
+++ src/components/text-field/text-field.js
@@ -0,0 +1,23 @@
+var element = document.getElementById('birthday');
+var maskOptions = {
+  mask: 'DD.MM.YY00',
+  blocks: {
+    DD: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 31
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: 19,
+      to: 20
+    },
+
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    }
+  }
+};
+var mask = IMask(element, maskOptions);
