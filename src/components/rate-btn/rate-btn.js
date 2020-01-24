9f7200ab496cffa00d3ace29e61f5d90463314c5
Lena <pechorin15@yandex.ru>
2020-01-24 22:21:55 +0400

    Ended Rate button

---
+++ src/components/rate-btn/rate-btn.js
@@ -0,0 +1,28 @@
+var allRateBtn = [];
+var allStars = [];
+
+for (var i=0; i<document.querySelectorAll('.rate-btn').length; i++) {
+  allRateBtn[i]= document.querySelectorAll('.rate-btn')[i];
+  allRateBtn[i].addEventListener('mouseover', onRateBtn)
+}
+
+function onRateBtn() {
+  for (var j=0; j<this.querySelectorAll('.rate-btn__star').length; j++) {
+    allStars[j]= this.querySelectorAll('.rate-btn__star')[j];
+    allStars[j].addEventListener('click', toggleStar);
+  }
+}
+
+function toggleStar() {
+  console.log(allStars.indexOf(this))
+  if (this.textContent == 'star_border'){
+    this.innerHTML = 'star';
+    for (var a=allStars.indexOf(this)+1; a<=4; a++) {
+      allStars[a].innerHTML = 'star';
+    }
+  } else {
+    for (var b=0; allStars[b]!=this; b++) {
+      allStars[b].innerHTML = 'star_border';
+    }
+  }
+}
