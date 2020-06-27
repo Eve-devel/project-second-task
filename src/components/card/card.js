9f17beb9a23858634aa51362e8ec94abe1077630
Lena <pechorin15@yandex.ru>
2020-06-27 21:36:50 +0300

    Room cards

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,5 +7,14 @@ import '../layout/layout.js'
 import '../text-field/text-field.js'
 import '../toggle/toggle.js'
 import '../radio/radio.js'
+import '../rate-btn/rate-btn.js'
 
-//$('.first-date-dropdown').data('datepicker').selectDate([new Date(2019, 7, 19), new Date(2019, 7, 23)])
+
+// $('.first-date-dropdown').eq(1).datepicker({
+//   position: "right top",
+//   inline: true,
+//   startDate: new Date(2019, 7, 8),
+//
+// });
+//
+// $('.first-date-dropdown:eq(0)').data('datepicker').selectDate([new Date(2019, 7, 20), new Date(2019, 7, 23)])
--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -3,28 +3,6 @@ import './plugin/datepicker.js';
 import IMask from 'imask';
 
 
-$('.first-date-dropdown').datepicker({
-
-        altField: '.filter-date-dropdown__input',
-        altFieldDateFormat: 'd M',
-        toggleSelected: false,
-        prevHtml: '<span class="material-icons">arrow_back</span>',
-        nextHtml: '<span class="material-icons">arrow_forward</span>',
-        multipleDates: true,
-        multipleDatesSeparator: ' - ',
-        range: true,
-        clearButton: true,
-        todayButton: true,
-        //separation date of first and second dropdowns
-        onSelect: (formattedDate) => {
-                    if (formattedDate.length > 1) {
-                      $('.first-date-dropdown').val(formattedDate.split(' - ')[0]);
-                      $('.second-date-dropdown').val(formattedDate.split(' - ')[1]);
-                    }
-                  },
-        language: {today: 'Применить'},
-});
-
 
 var element1 = document.querySelector('.first-date-dropdown');
 var element2 = document.querySelector('.second-date-dropdown');
